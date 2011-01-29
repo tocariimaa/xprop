1f20efb09b0d437e0618b7588212e2e0b9ffd3b2
Alan Coopersmith <alan.coopersmith@oracle.com>
2011-01-28 20:06:16 -0800

    Use _X_NORETURN from xproto 7.0.17 instead of local check
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Reviewed-by: Julien Cristau <jcristau@debian.org>

--- dsimple.h
+++ dsimple.h
@@ -37,6 +37,8 @@ from The Open Group.
  * Send bugs, etc. to chariot@athena.mit.edu.
  */
 
+#include <X11/Xfuncproto.h>
+
     /* Simple helper macros */
 #ifndef MAX
 #define MAX(a,b) (((a)>(b))?(a):(b))
@@ -77,8 +79,5 @@ void usage(void);
 
 Window Select_Window(Display *, int descend);
 Window Window_With_Name(Display *, Window, const char *);
-#ifdef __GNUC__
-void Fatal_Error(char *, ...) __attribute__((__noreturn__));
-#else
-void Fatal_Error(char *, ...);
-#endif
+
+void Fatal_Error(char *, ...) _X_NORETURN;
